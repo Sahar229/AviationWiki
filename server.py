17e0d344a4aa604cd8b3a18de939b98613dbabf0
Sahar_ <sahben229@gmail.com>
2026-02-09 11:17:07 +0200

    login register pages design

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for, flash
 
 app = Flask(__name__)
 
@@ -9,7 +9,46 @@ def home():
 
 @app.route("/learning")
 def learning_page():
-    return render_template("learning.html")
+    return render_template("learning_page.html")
+
+
+@app.route("/login", methods=['GET', 'POST'])
+def login_page():
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+        print(email,password)
+        
+        # TODO: כאן נוסיף את הקוד שמתחבר לשרת ה-Database
+        # 1. יצירת Socket
+        # 2. שליחת הודעה: "LOGIN|{email}|{password}"
+        # 3. קבלת תשובה מהשרת השני
+        
+        # בינתיים נדפיס לקונסול כדי לראות שהטופס עובד
+        print(f"Login attempt: {email}, {password}")
+        
+
+        return redirect(url_for('home'))
+        
+    return render_template("login_page.html")
+
+@app.route("/register", methods=['GET', 'POST'])
+def register_page():
+    if request.method == 'POST':
+        username = request.form['username']
+        email = request.form['email']
+        password = request.form['password']
+        print((username,email,password))
+        
+        # TODO: כאן נוסיף את הקוד שמתחבר לשרת ה-Database
+        # 1. יצירת Socket
+        # 2. שליחת הודעה: "REGISTER|{username}|{email}|{password}"
+        # 3. בדיקה אם המשתמש כבר קיים ושמירה
+        
+        print(f"Register attempt: {username}, {email}, {password}")
+        return redirect(url_for('login'))
+        
+    return render_template("register_page.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
